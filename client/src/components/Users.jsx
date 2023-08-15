f74f2561f480dc7ad677e7835dbc5ff7a5723079
Fayaz <fayazazeez2@gmail.com>
2023-08-15 09:47:36 +0530

    front-end done

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Users() {
+    const [users,setUsers] = useState([{
+        Name: "Fayaz", Email: "fayaz@gmail.com", Age: 21, 
+    }])
   return (
-    <div>
-        User
+    <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+        <div className='w-50 bg-white rounded p-3'>
+            <Link to='/create' className='btn btn-success'>Add User</Link>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Age</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user)=>{
+                               return <tr>
+                                    <td>{user.Name}</td>
+                                    <td>{user.Email}</td>
+                                    <td>{user.Age}</td>
+                                    <td>
+                                    <Link to='/update' className='btn btn-success'>Update</Link>
+                                        <button>Delete</button>
+                                    </td>
+                                </tr>
+                            })
+                        }
+
+                    </tbody>
+                </table>
+        </div>
     </div>
   )
 }
